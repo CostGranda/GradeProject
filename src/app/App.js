b6ea2c1ff59ecf8d2250981f7a7a87615ac82ebb
David Rojas <david_rojas82142@elpoli.edu.co>
2019-11-16 16:44:12 -0500

    routes

--- src/app/App.js
+++ src/app/App.js
@@ -2,9 +2,8 @@ import React, { Component, Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Menu from "./components/Menu";
 import Footer from "./components/Footer";
-import Login from "./pages/Login";
-import ApplicantForm from "./pages/ApplicantForm";
 import "./CSS/styles.css";
+import Routes from "./components/Routes/Routes";
 
 export default class App extends Component {
   render() {
@@ -12,10 +11,7 @@ export default class App extends Component {
       <Fragment>
         <Router>
           <Menu />
-          <Switch>
-            <Route exact path="/" component={Login} />
-            <Route exact path="/registre" component={ApplicantForm} />
-          </Switch>
+            <Routes authed={true} />
           <Footer />
         </Router>
       </Fragment>
