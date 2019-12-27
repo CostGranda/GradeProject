d447d00cc7fd195d820f5a2deb06fd8c96104842
David Rojas <david_rojas82142@elpoli.edu.co>
2019-12-26 22:23:06 -0500

    logout

--- src/app/App.js
+++ src/app/App.js
@@ -24,7 +24,7 @@ export default class App extends Component {
 
   componentDidMount() {
     const isAuted = localServices.getCurrentAccountId("user");
-    if (isAuted.usuario) {
+    if (isAuted && isAuted.usuario) {
       this.setState({ authed: true });
     }
   }
